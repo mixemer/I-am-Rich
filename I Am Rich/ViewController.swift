fdc62ddfc550ecea29475b82ee99753f66e83f5e
mixemer <mehmetshin96@gmail.com>
2019-01-22 16:32:54 -0500

    First IOS app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mehmet Sahin on 1/5/19.
+//  Copyright © 2019 Mehmet Sahin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
